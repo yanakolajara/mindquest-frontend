6781a2da4ca05444cf6362b70e21fcba07632a1b
yanakolajara <yanakolajara@pursuit.org>
2024-08-02 23:39:38 -0400

    setup: main components

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+type Props = {};
+
+export default function Navbar(props: Props): JSX.Element {
+  return (
+    <nav>
+      <h1>MindQuest</h1>
+      <button>Log in</button>
+    </nav>
+  );
+}
---
+++ src/routes/Home/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import GameCard from './GameCard';
+type Props = {};
+
+export default function Home(props: Props): JSX.Element {
+  return (
+    <main>
+      <GameCard />
+    </main>
+  );
+}
