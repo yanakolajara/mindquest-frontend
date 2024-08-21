315c506c05c5f9c2683dc6d55daa2dada16b4ba3
yanakolajara <yanakolajara@pursuit.org>
2024-08-21 11:35:03 -0400

    feature: memory cards logic moved to a custom hook

--- src/routes/Games/MemoryCards/MemoryCards.tsx
+++ src/routes/Games/MemoryCards/MemoryCards.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import Card from './Card';
+import { useCards } from './useCards';
 import './MemoryCards.scss';
 
 interface Card {
@@ -9,47 +10,7 @@ interface Card {
 }
 
 const MemoryGame = () => {
-  const [cards, setCards] = useState([
-    { id: 1, value: 'A', flipped: false },
-    { id: 2, value: 'A', flipped: false },
-    { id: 3, value: 'B', flipped: false },
-    { id: 4, value: 'B', flipped: false },
-    { id: 5, value: 'C', flipped: false },
-    { id: 6, value: 'C', flipped: false },
-  ]);
-
-  const [selectedCards, setSelectedCards] = useState<Card[]>([]);
-  const [matchedCards, setMatchedCards] = useState<Card[]>([]);
-
-  const handleFlip = (card: Card) => {
-    if (selectedCards.length < 2) {
-      setSelectedCards([...selectedCards, card]);
-      setCards(
-        cards.map((c) => (c.id === card.id ? { ...c, flipped: true } : c))
-      );
-    }
-  };
-
-  const handleMatch = () => {
-    if (selectedCards.length === 2) {
-      const [card1, card2] = selectedCards;
-      if (card1.value === card2.value) {
-        setMatchedCards([...matchedCards, card1, card2]);
-        setSelectedCards([]);
-      } else {
-        setTimeout(() => {
-          setCards(
-            cards.map((c) =>
-              c.id === card1.id || c.id === card2.id
-                ? { ...c, flipped: false }
-                : c
-            )
-          );
-          setSelectedCards([]);
-        }, 1000);
-      }
-    }
-  };
+  const { cards, selectedCards, handleFlip, handleMatch } = useCards();
 
   return (
     <div className='game-screen'>
--- src/routes/Games/MemoryCards/useCards.ts
+++ src/routes/Games/MemoryCards/useCards.ts
@@ -1,22 +1,59 @@
 import React from 'react';
 
-type CardType = {
+interface Card {
   id: number;
   value: string;
-  state: string;
-};
+  flipped: boolean;
+}
 
 const useCards = () => {
-  const [lockBoard, setLockBoard] = React.useState<boolean>(false);
-  let selectedCards: CardType[] = [];
-  let selectedCards2: CardType[] = [];
-  const flipCard = ({ id, value, state }: CardType) => {};
+  const [cards, setCards] = React.useState([
+    { id: 1, value: 'A', flipped: false },
+    { id: 2, value: 'A', flipped: false },
+    { id: 3, value: 'B', flipped: false },
+    { id: 4, value: 'B', flipped: false },
+    { id: 5, value: 'C', flipped: false },
+    { id: 6, value: 'C', flipped: false },
+  ]);
+  const [selectedCards, setSelectedCards] = React.useState<Card[]>([]);
+  const [matchedCards, setMatchedCards] = React.useState<Card[]>([]);
 
-  React.useEffect(() => {}, [lockBoard]);
+  const handleFlip = (card: Card) => {
+    if (selectedCards.length < 2) {
+      setSelectedCards([...selectedCards, card]);
+      setCards(
+        cards.map((c) => (c.id === card.id ? { ...c, flipped: true } : c))
+      );
+    }
+  };
+
+  const handleMatch = () => {
+    if (selectedCards.length === 2) {
+      const [card1, card2] = selectedCards;
+      if (card1.value === card2.value) {
+        setMatchedCards([...matchedCards, card1, card2]);
+        setSelectedCards([]);
+      } else {
+        setTimeout(() => {
+          setCards(
+            cards.map((c) =>
+              c.id === card1.id || c.id === card2.id
+                ? { ...c, flipped: false }
+                : c
+            )
+          );
+          setSelectedCards([]);
+        }, 1000);
+      }
+    }
+  };
+
+  React.useEffect(() => {}, []);
   return {
-    lockBoard,
-    setLockBoard,
-    flipCard,
+    cards,
+    handleFlip,
+    selectedCards,
+    handleMatch,
   };
 };
 
