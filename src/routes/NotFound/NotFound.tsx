980bfac86b0caac6b1ef7cb021bc14b82f627ed4
yanakolajara <yanakolajara@pursuit.org>
2024-08-30 12:53:14 -0400

    style: not found page structure

--- src/routes/NotFound/NotFound.tsx
+++ src/routes/NotFound/NotFound.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
+import './NotFound.scss';
 
 type Props = {};
 
-const NotFound = ({}: Props) => {
-  return <div>NotFound</div>;
+const NotFound = (props: Props) => {
+  return (
+    <div className='not-found'>
+      <h1>Page Not Found</h1>
+      <p>The page you are looking for does not exist</p>
+      <a href='/'>Go Home</a>
+    </div>
+  );
 };
 
 export default NotFound;
